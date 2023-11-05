084890288f84321b71fd585a4c0bdc33406a3882
Bakr-19 <141832833+Bakr-19@users.noreply.github.com>
2023-11-05 22:44:16 +0000

    Complete Now

--- app.js
+++ app.js
@@ -12,134 +12,114 @@ let display6 = document.querySelector('.display6')
 
 let displaybox = document.querySelector('.displaybox')
 
-let text = document.querySelector('.text')
+let text = document.querySelectorAll('.text')
+
+let display = document.querySelectorAll(".display")
+
 
 let audio = new Audio()
-let audio1 = new Audio()
-let audio2 = new Audio()
-let audio3 = new Audio()
-let audio4 = new Audio()
-let audio5 = new Audio()
 
 
 
 
+function btnDown(){
+  display1.addEventListener('click', () => {
+    myFunction(1)
+  })
+  display2.addEventListener('click', () => {
+    myFunction(2)
+  })
+  display3.addEventListener('click', () => {
+    myFunction(3)
+  })
+  display4.addEventListener('click', () => {
+    myFunction(4)
+  })
+  display5.addEventListener('click', () => {
+    myFunction(5)
+  })
+  display6.addEventListener('click', () => {
+    myFunction(6)
+  })
+
+  document.addEventListener('keydown', (event) => {
+    if(event.key == "d"){
+      myFunction(1)
+  
+    }
+    else if(event.key == "f"){
+      myFunction(2)
+    }
+    else if(event.key == "g"){
+      myFunction(3)
+    }
+    else if(event.key == "h"){
+      myFunction(4)
+    }
+    else if(event.key == "j"){
+      myFunction(5)
+    }
+    else if(event.key == "k"){
+      myFunction(6)
+    }
+  
+  })
+  
+}
+
+btnDown()
 
 
-
-display1.addEventListener('click', () => {
-  audio1.src = ("./sounds/boom.wav")
-  audio1.play(audio1)
-  displaybox.textContent = 'Boom'
-})
-
-
-document.addEventListener('keydown', (event) => {
-  audio1.src = ("./sounds/boom.wav")
-  if(event.key == "d"){
-    audio1.play(audio1)
-    displaybox.textContent = 'Boom'
-
-  }
-
-
-})
-
-
-display2.addEventListener('click', () => {
-  audio.src = ("./sounds/clap.wav")
-  audio.play(audio)
-  displaybox.textContent = 'Clap'
-})
-
-
-document.addEventListener('keydown', (event) => {
-  audio.src = ("./sounds/clap.wav")
-  audio.load(audio)
-  if(event.key == "f"){
-    audio.play(audio)
-    displaybox.textContent = 'Clap'
-
-  }
-
-})
-
-
-display3.addEventListener('click', () => {
-  audio2.src = ("./sounds/hihat.wav")
-  audio2.play(audio2)
-  displaybox.textContent = 'Hihat'
-})
-
-
-document.addEventListener('keydown', (event) => {
-  audio2.src = ("./sounds/hihat.wav")
-  audio2.load(audio2)
-  if(event.key == "g"){
-    audio2.play(audio2)
-    displaybox.textContent = 'Hihat'
-
-  }
-
-})
-
-
-display4.addEventListener('click', () => {
-  audio3.src = ("./sounds/kick.wav")
-  audio3.play(audio2)
-  displaybox.textContent = 'Kick'
-})
-
-
-document.addEventListener('keydown', (event) => {
-  audio3.src = ("./sounds/kick.wav")
-  audio3.load(audio3)
-  if(event.key == "h"){
-    audio3.play(audio3)
-    displaybox.textContent = 'Kick'
-
-  }
-
-})
-
-
-
-
-display5.addEventListener('click', () => {
-  audio4.src = ("./sounds/openhat.wav")
-  audio4.play(audio4)
-  displaybox.textContent = 'openhat'
-})
-
-
-document.addEventListener('keydown', (event) => {
-  audio4.src = ("./sounds/openhat.wav")
-  audio4.load(audio4)
-  if(event.key == "j"){
-    audio4.play(audio4)
-    displaybox.textContent = 'openhat'
+function myFunction(roll){
+  switch(roll){
+    case 1:
+      audio.setAttribute("src", "./sounds/boom.wav")
+      audio.play()
+      text[0].classList.toggle('special')
+      console.log(text[0].classList)
+      displaybox.textContent = 'Boom'
+      
+      break;
+    case 2:
+      audio.setAttribute("src", "./sounds/clap.wav")
+      console.log(audio)
+      audio.play()
+      text[1].classList.toggle('special')
+      displaybox.textContent = 'Clap'
+      break;
+    case 3:
+        audio.setAttribute("src", "./sounds/hihat.wav")
+        console.log(audio)
+        audio.play()
+        text[2].classList.toggle('special')
+        displaybox.textContent = 'Hihat'
+        break;
+    case 4:
+          audio.setAttribute("src", "./sounds/kick.wav")
+          console.log(audio)
+          audio.play()
+          text[3].classList.toggle('special')
+          displaybox.textContent = 'Kick'
+          
+          break;
+    case 5:
+            audio.setAttribute("src", "./sounds/openhat.wav")
+            console.log(audio)
+            audio.play()
+            text[4].classList.toggle('special')
+            displaybox.textContent = 'Openhat'
+            break;
+    case 6:
+              audio.setAttribute("src", "./sounds/ride.wav")
+              console.log(audio)
+              audio.play()
+              text[5].classList.toggle('special')
+              displaybox.textContent = 'Ride'
+              break;    
+    
+    default:
+      console.log("Nothing")
 
   }
-
-})
-
-
-
-display6.addEventListener('click', () => {
-  audio5.src = ("./sounds/ride.wav")
-  audio5.play(audio5)
-  displaybox.textContent = 'ride'
-})
-
-
-document.addEventListener('keydown', (event) => {
-  audio5.src = ("./sounds/ride.wav")
-  audio5.load(audio5)
-  if(event.key == "k"){
-    audio5.play(audio5)
-    displaybox.textContent = 'ride'
-
-  }
-
-})
+}
 
